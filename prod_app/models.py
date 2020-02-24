e5dc474bfa4b9d340a22acb459ac48ea611a2dce
bart3k1 <bart3k1@gmail.com>
2020-02-24 19:56:26 +0100

    added 1 vote limit

--- prod_app/models.py
+++ prod_app/models.py
@@ -13,10 +13,16 @@ class Product(models.Model):
     hunter = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def summary(self):
-        return self.body[:100] + "..."
+        return self.body[:200] + "..."
 
     def pub_date_pretty(self):
         return self.pub_date.strftime("%b %e %Y")
 
     def __str__(self):
         return self.title
+
+
+class Votes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
--- prod_app/views.py
+++ prod_app/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Product, Votes
 
 
 def home(request):
-    return render(request, "prod_app/home.html")
+    products = Product.objects.all().order_by("-votes_total")
+    return render(request, "prod_app/home.html", {"products": products})
+
+
+@login_required(login_url="/accounts/signup")
+def create(request):
+    if request.method == "POST":
+        if request.POST["title"] and request.POST["body"] and request.POST["url"] and request.FILES["icon"] and request.FILES["image"]:
+            product = Product()
+            product.title = request.POST["title"]
+            product.body = request.POST["body"]
+            if request.POST["url"].startswith("http://") or request.POST["url"].startswith("https://"):
+                product.url = request.POST["url"]
+            else:
+                product.url = "http://" + request.POST["url"]
+            product.icon = request.FILES["icon"]
+            product.image = request.FILES["image"]
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            #todo redirect to product deteails
+            return redirect("/products/" + str(product.id))
+
+        else:
+            return render(request, "prod_app/create", {"error": "All fields are required"})
+
+    else:
+        return render(request, "prod_app/create.html")
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    return render(request, "prod_app/detail.html", {"product": product})
+
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method == "POST":
+        if Votes.objects.filter(user=request.user.id, product=product_id).exists():
+            return redirect("/products/"+str(product_id))
+        else:
+            product = get_object_or_404(Product, pk=product_id)
+            product.votes_total += 1
+            product.save()
+            Votes(product=product, user=request.user).save()
+            return render(request, "prod_app/detail.html", {"product": product})
+
+    return redirect("/products/"+str(product_id))
+
